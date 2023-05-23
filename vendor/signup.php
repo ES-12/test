36c8560fc3f6253b3046ad00a538e36934e4b816
es-12 <evgenyseleznev12@gmail.com>
2023-05-23 23:40:38 +0300

    add a uniqueness check of entered data

--- vendor/signup.php
+++ vendor/signup.php
@@ -14,17 +14,39 @@ $password_confirm = $_POST['password_confirm'];
 if ($password === $password_confirm) {
     $password = md5($password);
 
-    mysqli_query($connection, "INSERT INTO `users`
+    /*проверка на отсутствие в БД
+    имени пользователя, номера телефона и почты при регистрации
+    и отправка сообщения, если имя, телефон или почта уже заняты*/
+
+    if (mysqli_query($connection, "SELECT name FROM users WHERE name = '$name'")) {
+
+        $_SESSION['message'] = 'Пользователь c таким именем уже существует';
+
+        header('Location: ../registration.php');
+    } elseif (mysqli_query($connection, "SELECT phone_number FROM users WHERE phone_number = '$phone_number'")) {
+
+        $_SESSION['message'] = 'Пользователь c таким телефоном уже существует';
+
+        header('Location: ../registration.php');
+    } elseif (mysqli_query($connection, "SELECT email FROM users WHERE email = '$email'")) {
+
+        $_SESSION['message'] = 'Пользователь c такой почтой уже существует';
+
+        header('Location: ../registration.php');
+    } else {
+
+        mysqli_query($connection, "INSERT INTO `users`
         (`id`, `name`, `phone_number`, `email`, `password`)
         VALUES (NULL, '$name', '$phone_number', '$email', '$password') ");
 
-    /*при успешной регистрации и занесении данных в БД
-    перенаправление на корневую страницу для авторизации
-    и вывод сообщения об успешности операции*/
+        /*при успешной регистрации и занесении данных в БД
+        перенаправление на корневую страницу для авторизации
+        и вывод сообщения об успешности операции*/
 
-    $_SESSION['message'] = 'Регистрация завершена';
+        $_SESSION['message'] = 'Регистрация завершена';
 
-    header('Location: ../index.php');
+        header('Location: ../index.php');
+    }
 } else {
     $_SESSION['message'] = 'Ошибка подтверждения пароля';
 
